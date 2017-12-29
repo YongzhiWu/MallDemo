040a1d238a6ffad04a2ae7fe260b8655cdd9f184
wxkhust <30928849+wxkhust@users.noreply.github.com>
2017-12-29 11:56:51 +0800

    忘记密码的修改
    更新了忘记密码部分的页面，采用的是新的forgetpassword.css文件做样式。另外注册登录页面有小的改动，可以点击的部分我加了一个反应：鼠标移动上去的时候会显示。

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -4,19 +4,19 @@
  * @type {Object}
  */
 export default {
-  type: 'mysql',
-  adapter: {
-    mysql: {
-      host: '127.0.0.1',  //localhost
-      port: '3306',   //服务器占用本地端口
-      database: 'malldemo',  //数据库名称
-      user: 'root',          //数据库用户名
-      password: '123456',    //数据库密码
-      prefix: '',
-      encoding: 'utf8'
-    },
-    mongo: {
+    type: 'mysql',
+    adapter: {
+        mysql: {
+            host: '127.0.0.1', //localhost
+            port: '3306', //服务器占用本地端口
+            database: 'malldemo1', //数据库名称
+            user: 'root', //数据库用户名
+            password: '123456', //数据库密码
+            prefix: '',
+            encoding: 'utf8'
+        },
+        mongo: {
 
+        }
     }
-  }
 };
--- src/user/controller/signup.js
+++ src/user/controller/signup.js
@@ -3,34 +3,38 @@
 import Base from './base.js';
 
 export default class extends Base {
-  /**
-   * index action
-   * @return {Promise} []
-   */
-  async indexAction(){
-    //auto render template file index_index.html
-    if(this.isPost()){
-      let username = this.post('username');
-      let password = this.post('password');
-      let phone = this.post('phone');
-      let mail = this.post('mail');
-      //生成用户Id，getTime() 方法可返回距 1970 年 1 月 1 日之间的毫秒数
-      var d = new Date();
-      let userid = d.getTime().toString();
-      let adduser = await this.model('user').add({
-        UserId: userid,
-        shopcartid: userid,
-        UserName:username, 
-        UserPwd:password,
-        UserEmail: mail,
-        UserTel: phone
-      });
-      let addcart = await this.model('shopcart').add({
-        ShopCartId: userid,
-        UserId: userid
-      });
-      return this.redirect('./login');
+    /**
+     * index action
+     * @return {Promise} []
+     */
+    async indexAction() {
+        //auto render template file index_index.html
+        if (this.isPost()) {
+            let username = this.post('username');
+            let password = this.post('password');
+            let phone = this.post('phone');
+            let mail = this.post('email');
+            let question = this.post('question');
+            let answer = this.post('answer');
+            //生成用户Id，getTime() 方法可返回距 1970 年 1 月 1 日之间的毫秒数
+            var d = new Date();
+            let userid = d.getTime().toString();
+            let adduser = await this.model('user').add({
+                UserId: userid,
+                shopcartid: userid,
+                UserName: username,
+                UserPwd: password,
+                UserEmail: mail,
+                UserTel: phone,
+                UserQuestion: question,
+                UserAnswer: answer
+            });
+            let addcart = await this.model('shopcart').add({
+                ShopCartId: userid,
+                UserId: userid
+            });
+            return this.redirect('./login');
+        }
+        return this.display();
     }
-    return this.display();
-  }
 }
